0603b7b4b5b35876af0a921d99c60179cd9749ed
Sinh Trung <hst2us@gmail.com>
2022-10-27 22:57:54 -0400

    Introduced Ridge regression

--- src/linear_regression.jl
+++ src/linear_regression.jl
@@ -3,13 +3,15 @@
     the β parameter
 =#
 hcat_feature_one(x) = hcat(ones(size(x)),x) #add ones to features to get β0
+
 function fit(x,y,regression = "Linear", λ = 0)
     X=hcat_feature_one(x)
     if regression == "Linear"
         return inv(X'*X)*X'*y
- #   elseif regression == "Ridge"
- #       Î = Diagonal(vcat([0],ones(size(X,1)))) #make diagonal identity array except at first diagonal
- #       return inv(X'*X.-λ*Î)*X'*y
+    elseif regression == "Ridge"
+        Î = Diagonal(vcat([0],ones(size(x,2)))) #make diagonal identity array except at first diagonal
+        println(size(Î),size(X'*X))
+        return inv(X'*X-λ*Î)*X'*y
     end
 end 
 
@@ -17,7 +19,7 @@ function loss(x,y,β,regression = "Linear", λ = 0)
     X=hcat_feature_one(x)
     if regression == "Linear"
         return 0.5(y-X*β)'*(y-X*β)
-#    elseif regression == "Ridge"
-#        return 0.5(y-ŷ)'*(y-ŷ)+0.5λ*sum(β^.2)
+    elseif regression == "Ridge"
+        return 0.5(y-X*β)'*(y-X*β)+0.5λ*sum(β^.2)
     end
 end 
